b112a8b5abc4e5f26e616706f7f84727336763aa
Your Name <anthonybrugal@yahoo.com>
2017-09-22 19:01:27 -0400

    dependencies or whatever

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('https://git.heroku.com/nameless-peak-90953.git/api/listings/');
+        return $http.get('https://whispering-refuge-14282.herokuapp.com/api/listings/');
       },
 
       create: function(listing) {
-        return $http.post('https://git.heroku.com/nameless-peak-90953.git/api/listings/', listing);
+        return $http.post('https://whispering-refuge-14282.herokuapp.com/api/listings/', listing);
       },
 
       read: function(id) {
-        return $http.get('https://git.heroku.com/nameless-peak-90953.git/api/listings/' + id);
+        return $http.get('https://whispering-refuge-14282.herokuapp.com/api/listings/' + id);
       },
 
       update: function(id, listing) {
-        return $http.put('https://git.heroku.com/nameless-peak-90953.git/api/listings/' + id, listing);
+        return $http.put('https://whispering-refuge-14282.herokuapp.com/api/listings/' + id, listing);
       },
 
       delete: function(id) {
-        return $http.delete('https://git.heroku.com/nameless-peak-90953.git/api/listings/' + id);
+        return $http.delete('https://whispering-refuge-14282.herokuapp.com/api/listings/' + id);
       }
     };
 
