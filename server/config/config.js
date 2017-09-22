67c8ca4be421819291e05110f057b819e054f72d
Your Name <anthonybrugal@yahoo.com>
2017-09-22 18:56:55 -0400

    added files

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://Brugal1122:Ab11221996!@ds119064.mlab.com:19064/ab_assignment3', //place the URI of your mongo database here.
+  },
+  googleMaps: {
+    key: 'AIzaSyCugyBVwSkxtX3qxbs2QQLC23oMop5aTy'
+  },
+  port: 8080
+};
